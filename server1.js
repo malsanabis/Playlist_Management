fb8c0b5068030747fbb979e9e068b45b54b86304
Mjhumaidan16 <mhumaidan08@gmail.com>
2025-08-31 14:29:36 +0300

    update

--- server1.js
+++ server1.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const router = require('./router');
-const { PORT } = require('./config');
 require('dotenv').config();
+const { PORT } = process.env;
 
 const server = http.createServer((req, res) => {
   router(req, res);
 });
 
-server.listen(3000, () => {
-  console.log(`Server running at http://localhost:${3000}`);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
